6a63744886254aca5635903cafdff918e5056fee
Noah Swartz <swartzcr@gmail.com>
2014-09-24 17:10:40 -0700

    added info and queue file and fixed bugs in bot_logic

---
+++ bot_logic.py
@@ -0,0 +1,42 @@
+from twython import Twython
+import json
+
+def auth():
+    with open("access.json", 'r') as f:
+        db = json.load(f)
+    return Twython(db["API_Key"], db["API_Secret"], db["Access_Token"], db["Access_Token_Secret"])
+
+def load():
+    with open("queue.json", 'r') as f:
+        queue = json.load(f)
+    with open("info.json", 'r') as fi:
+        info = json.load(fi)
+    return queue, info
+
+def dump(queue, info):
+    with open("queue.json", 'w') as f:
+        json.dump(queue, f)
+    with open("info.json", 'w') as fi:
+        json.dump(info, fi)
+
+def respond(twitter, top_tweet):
+    name = top_tweet["user"]["screen_name"]
+    twitter.update_status(status="@%s Oh, I can believe it!" %(name), in_reply_to_status_id=top_tweet["id"])
+
+
+def main():
+    twitter = auth()
+    queue, info = load()
+    print queue
+    print info
+    tweets = twitter.search(q="I can't believe", result_type="recent", since_id=info["sinceid"], count='100')
+    info["sinceid"] = tweets["search_metadata"]["max_id"]
+    queue = queue + [tweet for tweet in tweets["statuses"] if not tweet["retweeted"]]
+    print queue
+    if len(queue) > 0:
+        respond(twitter, queue.pop())
+    dump(queue, info)
+
+#run on cron every minute
+if __name__ == "__main__":
+    main()
